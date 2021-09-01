ccfd6cd1d006ed28aa6d80459d1088c231a84e00
Luis Luna <siul-luna@hotmail.com>
2021-09-01 16:11:50 -0500

    Final

---
+++ CodigoQR/CatchingEvents.vb
@@ -0,0 +1,214 @@
+﻿Friend Class CatchingEvents
+
+    Friend WithEvents SBOApplication As SAPbouiCOM.Application '//OBJETO DE APLICACION
+    Friend SBOCompany As SAPbobsCOM.Company '//OBJETO COMPAÑIA
+    Friend csDirectory As String '//DIRECTORIO DONDE SE ENCUENTRAN LOS .SRF
+
+    Public Sub New()
+        MyBase.New()
+        SetAplication()
+        SetConnectionContext()
+        ConnectSBOCompany()
+
+        setFilters()
+
+    End Sub
+
+    '//----- ESTABLECE LA COMUNICACION CON SBO
+    Private Sub SetAplication()
+        Dim SboGuiApi As SAPbouiCOM.SboGuiApi
+        Dim sConnectionString As String
+        Try
+            SboGuiApi = New SAPbouiCOM.SboGuiApi
+            sConnectionString = Environment.GetCommandLineArgs.GetValue(1)
+            SboGuiApi.Connect(sConnectionString)
+            SBOApplication = SboGuiApi.GetApplication()
+        Catch ex As Exception
+            SBOApplication.MessageBox("Falló la conexión con la aplicación SBO " & ex.Message)
+            System.Windows.Forms.Application.Exit()
+            End '//termina aplicación
+        End Try
+    End Sub
+
+    '//----- ESTABLECE EL CONTEXTO DE LA APLICACION
+    Private Sub SetConnectionContext()
+        Try
+            SBOCompany = SBOApplication.Company.GetDICompany
+        Catch ex As Exception
+            SBOApplication.MessageBox("Falló la conexión con el DI")
+            SBOApplication = Nothing
+            System.Windows.Forms.Application.Exit()
+            End '//termina aplicación
+            'Finally
+        End Try
+    End Sub
+
+    '//----- CONEXION CON LA BASE DE DATOS
+    Private Sub ConnectSBOCompany()
+        Dim loRecSet As SAPbobsCOM.Recordset
+        Try
+            '//ESTABLECE LA CONEXION A LA COMPAÑIA
+            csDirectory = My.Application.Info.DirectoryPath
+            If (csDirectory = "") Then
+                System.Windows.Forms.Application.Exit()
+                End
+            End If
+        Catch ex As Exception
+            SBOApplication.MessageBox("Falló la conexión con la BD. " & ex.Message)
+            SBOApplication = Nothing
+            System.Windows.Forms.Application.Exit()
+            End '//termina aplicación
+        Finally
+            loRecSet = Nothing
+        End Try
+    End Sub
+
+    '//----- ESTABLECE FILTROS DE EVENTOS DE LA APLICACION
+    Private Sub setFilters()
+        Dim lofilter As SAPbouiCOM.EventFilter
+        Dim lofilters As SAPbouiCOM.EventFilters
+
+        Try
+            lofilters = New SAPbouiCOM.EventFilters
+            lofilter = lofilters.Add(SAPbouiCOM.BoEventTypes.et_ITEM_PRESSED)
+            lofilter.AddEx(133) '// FORMA Factura clientes
+
+            SBOApplication.SetFilter(lofilters)
+
+        Catch ex As Exception
+            SBOApplication.MessageBox("SetFilter: " & ex.Message)
+        End Try
+
+    End Sub
+
+    ''//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ''// METODOS PARA EVENTOS DE LA APLICACION
+    ''//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    Private Sub SBO_Application_AppEvent(ByVal EventType As SAPbouiCOM.BoAppEventTypes) Handles SBOApplication.AppEvent
+        Select Case EventType
+            Case SAPbouiCOM.BoAppEventTypes.aet_ShutDown, SAPbouiCOM.BoAppEventTypes.aet_ServerTerminition, SAPbouiCOM.BoAppEventTypes.aet_CompanyChanged
+                System.Windows.Forms.Application.Exit()
+                End
+        End Select
+
+    End Sub
+
+    '//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    '// METODOS PARA MANEJO DE EVENTOS ITEM
+    '//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    Private Sub SBOApplication_ItemEvent(ByVal FormUID As String, ByRef pVal As SAPbouiCOM.ItemEvent, ByRef BubbleEvent As Boolean) Handles SBOApplication.ItemEvent
+        Try
+
+            If pVal.Before_Action = True And pVal.FormTypeEx <> "" Then
+                Select Case pVal.FormTypeEx
+
+                    Case 133    '////// FORMA Factura clientes
+                        frmOINVControllerBefore(FormUID, pVal)
+
+                End Select
+            End If
+
+            If pVal.Before_Action = False And pVal.FormTypeEx <> "" Then
+                Select Case pVal.FormTypeEx
+
+                    Case 133    '////// FORMA Factura clientes
+                        frmOINVControllerAfter(FormUID, pVal)
+
+                End Select
+            End If
+
+        Catch ex As Exception
+            SBOApplication.MessageBox("SBOApplication_ItemEvent. ItemEvent " & ex.Message)
+        Finally
+        End Try
+    End Sub
+
+
+    '//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    '// CONTROLADOR DE EVENTOS FORMA SN
+    '//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    Private Sub frmOINVControllerBefore(ByVal FormUID As String, ByVal pVal As SAPbouiCOM.ItemEvent)
+
+        Dim oOINV As CrearQR
+        Dim coForm As SAPbouiCOM.Form
+        Dim stTabla As String
+        Dim oDatatable As SAPbouiCOM.DBDataSource
+        Dim DocNum, DocDate As String
+
+        Try
+
+            Select Case pVal.EventType
+
+                '//////Evento Presionar Item
+                Case SAPbouiCOM.BoEventTypes.et_ITEM_PRESSED
+
+                    Select Case pVal.ItemUID
+
+                        Case "1"
+
+                            stTabla = "OINV"
+                            coForm = SBOApplication.Forms.Item(FormUID)
+                            oDatatable = coForm.DataSources.DBDataSources.Item(stTabla)
+
+                            DocNum = Nothing
+                            DocDate = Nothing
+
+                            DocNum = oDatatable.GetValue("DocNum", 0)
+                            DocDate = oDatatable.GetValue("DocDate", 0)
+
+                            oOINV = New CrearQR
+                            oOINV.CreateQR(DocNum, DocDate)
+
+                    End Select
+
+            End Select
+
+        Catch ex As Exception
+            SBOApplication.MessageBox("Error en el evento sobre Forma Factura Ventas. " & ex.Message)
+        Finally
+            'oPO = Nothing
+        End Try
+    End Sub
+
+    Private Sub frmOINVControllerAfter(ByVal FormUID As String, ByVal pVal As SAPbouiCOM.ItemEvent)
+
+        Dim oOINV As CrearQR
+        Dim coForm As SAPbouiCOM.Form
+        Dim stTabla As String
+        Dim oDatatable As SAPbouiCOM.DBDataSource
+        Dim DocNum As String
+
+        Try
+
+            Select Case pVal.EventType
+
+                '//////Evento Presionar Item
+                Case SAPbouiCOM.BoEventTypes.et_ITEM_PRESSED
+
+                    Select Case pVal.ItemUID
+
+                        Case "1"
+
+                            stTabla = "OINV"
+                            coForm = SBOApplication.Forms.Item(FormUID)
+                            oDatatable = coForm.DataSources.DBDataSources.Item(stTabla)
+
+                            DocNum = Nothing
+                            DocNum = oDatatable.GetValue("DocNum", 0) - 1
+
+                            oOINV = New CrearQR
+                            oOINV.ValidarImg(DocNum)
+
+                    End Select
+
+            End Select
+
+        Catch ex As Exception
+            SBOApplication.MessageBox("Error en el evento sobre Forma Factura Ventas. " & ex.Message)
+        Finally
+            'oPO = Nothing
+        End Try
+    End Sub
+
+End Class
---
+++ CodigoQR/CrearQR.vb
@@ -0,0 +1,145 @@
+﻿Imports System.Net
+Imports System.IO
+Imports System.Drawing
+
+Public Class CrearQR
+
+    Private cSBOApplication As SAPbouiCOM.Application '//OBJETO DE APLICACION
+    Private cSBOCompany As SAPbobsCOM.Company
+
+
+    '//----- METODO DE CREACION DE LA CLASE
+    Public Sub New()
+        MyBase.New()
+        cSBOApplication = oCatchingEvents.SBOApplication
+        cSBOCompany = oCatchingEvents.SBOCompany
+    End Sub
+
+
+    Public Function CreateQR(ByVal DocNum As String, ByVal DocDate As String)
+
+        Dim stQueryH As String
+        Dim oRecSetH As SAPbobsCOM.Recordset
+        Dim URL As String
+        Dim QRPath, QRSaved As String
+        Dim webclient As WebClient
+        Dim stream As Stream
+        Dim bitmap As Bitmap
+
+        oRecSetH = cSBOCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoRecordset)
+
+        Try
+            '///URL codificacion de caracteres https://zainex.es/guia-rapida/html/html-url-encode-codificacion
+            stQueryH = "Select ""BitmapPath"" from OADP"
+            oRecSetH.DoQuery(stQueryH)
+
+            If oRecSetH.RecordCount > 0 Then
+
+                QRPath = oRecSetH.Fields.Item("BitmapPath").Value.ToString & "Codigos QR\"
+                DocDate = DocDate.Substring(6, 2) & "/" & DocDate.Substring(4, 2) & "/" & DocDate.Substring(0, 4)
+
+                webclient = New WebClient()
+                URL = "https://www.algoryt.com/qr/qr-generator.php?" & DocNum & "%09" & DocDate & "%09%09"
+
+                stream = webclient.OpenRead(URL)
+                bitmap = New Bitmap(stream)
+
+                If bitmap IsNot Nothing Then
+
+                    QRSaved = Nothing
+                    QRSaved = QRPath & DocNum & ".png"
+                    bitmap.Save(QRSaved, System.Drawing.Imaging.ImageFormat.Png)
+
+                End If
+
+            End If
+
+        Catch ex As Exception
+
+            cSBOApplication.MessageBox("Error en CreateQR. " & ex.Message)
+
+        End Try
+
+    End Function
+
+    Public Function ValidarImg(ByVal DocNum As String)
+
+        Dim Ruta, qr As String
+        Dim stQueryH As String
+        Dim oRecSetH As SAPbobsCOM.Recordset
+        qr = Nothing
+
+        oRecSetH = cSBOCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoRecordset)
+
+        Try
+
+            stQueryH = "Select ""BitmapPath"" from OADP"
+            oRecSetH.DoQuery(stQueryH)
+
+            If oRecSetH.RecordCount > 0 Then
+
+                Ruta = oRecSetH.Fields.Item("BitmapPath").Value.ToString & "Codigos QR\"
+
+                Dim dir As New System.IO.DirectoryInfo(Ruta)
+
+                Dim fileList = dir.GetFiles("*.png", System.IO.SearchOption.TopDirectoryOnly)
+
+                Dim FileQuery = From file In fileList
+                                Where file.Extension = ".png" And file.Name.Trim.ToString.EndsWith(DocNum & ".png") And file.Name.Trim.ToString.StartsWith(DocNum & ".png")
+                                Order By file.CreationTime
+                                Select file
+
+                qr = Ruta & DocNum & ".png"
+
+                If FileQuery.Count > 0 Then
+
+                    UpdateQR(DocNum, qr)
+
+                End If
+
+            End If
+
+        Catch ex As Exception
+
+            cSBOApplication.MessageBox("Error en ValidarImg. " & ex.Message)
+
+        End Try
+
+    End Function
+
+    Public Function UpdateQR(ByVal DocNum As String, ByVal qr As String)
+
+        Dim stQueryH, stQueryH2 As String
+        Dim oRecSetH, oRecSetH2 As SAPbobsCOM.Recordset
+        Dim QRField As String
+
+        oRecSetH = cSBOCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoRecordset)
+        oRecSetH2 = cSBOCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoRecordset)
+
+        Try
+
+            stQueryH = "Select ""U_QRImage"" from OINV where ""DocNum""=" & DocNum
+            oRecSetH.DoQuery(stQueryH)
+
+            If oRecSetH.RecordCount = 1 Then
+
+                QRField = oRecSetH.Fields.Item("U_QRImage").Value
+
+                If QRField = Nothing Then
+
+                    stQueryH2 = "Update OINV set ""U_QRImage""='" & qr & "' where ""DocNum""=" & DocNum
+                    oRecSetH2.DoQuery(stQueryH2)
+
+                End If
+
+            End If
+
+        Catch ex As Exception
+
+            cSBOApplication.MessageBox("Error en UpdateQR. " & ex.Message)
+
+        End Try
+
+    End Function
+
+End Class
